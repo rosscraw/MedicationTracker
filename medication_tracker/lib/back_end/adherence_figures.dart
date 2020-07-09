d81446ffa8b77f7e8b076339efdf5b95d8ebe39e
Ross Crawford <ross.crawford.2013@uni.strath.ac.uk>
2020-07-09 12:38:25 +0100

    Began working on adherence back end.

---
+++ medication_tracker/lib/back_end/adherence_figures.dart
@@ -0,0 +1,30 @@
+
+import 'package:medicationtracker/back_end/medication_regime.dart';
+import 'package:medicationtracker/back_end/user.dart';
+
+import 'dose_time_details.dart';
+
+class AdherenceFigures {
+  int _total = 0;
+  int _taken = 0;
+  User user;
+  List<MedicationRegime> medications;
+
+  int getTakenMeds() {
+    return _taken;
+  }
+
+  void setTakenMeds(int taken) {
+    _taken = taken;
+  }
+
+  int getTotalMeds() {
+    for (MedicationRegime medication in user.getMedicationList()) {
+      for (DoseTimeDetails time in medication.dosageTimings) {
+        _total++;
+      }
+      return _total;
+    }
+  }
+
+}
