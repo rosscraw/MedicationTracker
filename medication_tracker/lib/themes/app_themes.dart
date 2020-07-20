365e3d6806a871566ed228db39ceb7210411377e
Ross Crawford <ross.crawford.2013@uni.strath.ac.uk>
2020-07-20 21:42:46 +0100

    Updated dark and light app themes.

--- medication_tracker/lib/themes/app_themes.dart
+++ medication_tracker/lib/themes/app_themes.dart
@@ -3,50 +3,86 @@ import 'package:flutter/material.dart';
 class AppTheme {
   AppTheme._();
 
-  static Color _iconColor = Colors.blueAccent.shade200;
+  static Color _iconColor = Colors.white;
 
-  static const Color _lightPrimaryColor = Colors.blue;
-  static const Color _lightPrimaryVariantColor = Colors.blue;
-  static const Color _lightSecondaryColor = Colors.blueAccent;
-  static const Color _lightOnPrimaryColor = Colors.white;
 
-  static const Color _darkPrimaryColor = Colors.white24;
-  static const Color _darkPrimaryVariantColor = Colors.black;
+
+  // Backgrounds
+  static const Color _lightPrimaryColor = Colors.white;
+  // Cards
+  static const Color _lightPrimaryVariantColor = Colors.white;
+  // Buttons
+  static const Color _lightSecondaryColor = Colors.blue;
+  static const Color _lightSecondaryVariantColor = Colors.white;
+  // Text on background
+  static const Color _lightOnPrimaryColor = Colors.blue;
+  // Text on cards/ buttons
+  static const Color _lightOnSecondaryColor = Colors.white;
+
+  static const Color _darkPrimaryColor = Colors.black;
+  static const Color _darkPrimaryVariantColor = Colors.white24;
   static const Color _darkSecondaryColor = Colors.white;
+  static const Color _darkSecondaryVariantColor = Colors.white;
   static const Color _darkOnPrimaryColor = Colors.white;
+  static const Color _darkOnSecondaryColor = Colors.blue;
 
   static final ThemeData lightTheme = ThemeData(
-    scaffoldBackgroundColor: _lightPrimaryVariantColor,
+    scaffoldBackgroundColor: _lightPrimaryColor,
     appBarTheme: AppBarTheme(
-      color: _lightPrimaryVariantColor,
-      iconTheme: IconThemeData(color: _lightOnPrimaryColor),
+      color: _lightSecondaryColor,
+      iconTheme: IconThemeData(color: _lightOnSecondaryColor),
     ),
     colorScheme: ColorScheme.light(
       primary: _lightPrimaryColor,
       primaryVariant: _lightPrimaryVariantColor,
       secondary: _lightSecondaryColor,
+      secondaryVariant: _lightSecondaryVariantColor,
       onPrimary: _lightOnPrimaryColor,
+      onSecondary: _lightOnSecondaryColor,
     ),
     iconTheme: IconThemeData(
-      color: _iconColor,
+      color: Colors.blue,
+    ),
+    primaryIconTheme: IconThemeData(
+      color: Colors.blue
+    ),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: _lightSecondaryColor,
+        foregroundColor: _lightOnSecondaryColor
+    ),
+    buttonTheme: ButtonThemeData(
+        buttonColor: _lightSecondaryColor
+    ),
+    cardTheme: CardTheme(
+      color: Colors.white70,
+      elevation: 5
     ),
     textTheme: _lightTextTheme,
   );
 
   static final ThemeData darkTheme = ThemeData(
-    scaffoldBackgroundColor: _darkPrimaryVariantColor,
+//    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+//      selectedItemColor: Colors.blue
+//    ),
+    scaffoldBackgroundColor: _darkPrimaryColor,
     appBarTheme: AppBarTheme(
       color: _darkPrimaryVariantColor,
       iconTheme: IconThemeData(color: _darkOnPrimaryColor),
     ),
     colorScheme: ColorScheme.dark(
-      primary: _darkPrimaryColor,
-      primaryVariant: _darkPrimaryVariantColor,
-      secondary: _darkSecondaryColor,
-      onPrimary: _darkOnPrimaryColor,
     ),
     iconTheme: IconThemeData(
       color: _iconColor,
+    ),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: _darkPrimaryVariantColor,
+        foregroundColor: _darkOnPrimaryColor
+    ),
+    buttonTheme: ButtonThemeData(
+      buttonColor: _darkPrimaryVariantColor
+    ),
+    cardTheme: CardTheme(
+        elevation: 5
     ),
     textTheme: _darkTextTheme,
   );
@@ -65,9 +101,9 @@ class AppTheme {
 
   static final TextStyle _lightScreenHeadingTextStyle = TextStyle(fontSize: 30.0, color: _lightOnPrimaryColor);
   static final TextStyle _lightScreenTaskNameTextStyle = TextStyle(fontSize: 20.0, color: _lightOnPrimaryColor);
-  static final TextStyle _lightScreenTaskDurationTextStyle = TextStyle(fontSize: 16.0, color: Colors.grey);
+  static final TextStyle _lightScreenTaskDurationTextStyle = TextStyle(fontSize: 15.0, color: Colors.black);
 
   static final TextStyle _darkScreenHeadingTextStyle = _lightScreenHeadingTextStyle.copyWith(color: _darkOnPrimaryColor);
   static final TextStyle _darkScreenTaskNameTextStyle = _lightScreenTaskNameTextStyle.copyWith(color: _darkOnPrimaryColor);
-  static final TextStyle _darkScreenTaskDurationTextStyle = _lightScreenTaskDurationTextStyle;
+  static final TextStyle _darkScreenTaskDurationTextStyle = TextStyle(fontSize: 15.0, color: Colors.white);
 }
